c035b1bc2b272956df78fadb26fa0c9f98af922f
James Njenga <james.njenga@strathmore.edu>
2021-06-28 12:42:03 +0300

    Lexer : Update unknown tokens token types

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,20 +1,26 @@
+// TODO : Is this the correct way of including the file?
+use crate::isa;
+
 #[allow(dead_code)]
 #[derive(PartialEq,Eq)]
 #[derive(Debug)]
 pub enum TokenType
 {
     WHITE,
-    NL,
-    EOF,
-    NUMBER,
     HASH,
+    COLON,
     DOLLAR,
     PERCENT,
-    COLON,
     COMMA,
-    INSTRUCTION,
-    OPERAND,
+    BRACKETOPEN,
+    BRACKETCLOSE,
+    NL,
     UNKNOWN,
+    INSTRUCTION,
+    NUMBER,
+    OPERAND,
+    LABEL,
+    EOF,
 
 }
 
@@ -45,16 +51,23 @@ impl Lexer
         Some(&self.tokens[self.current_token - 1])
     }
 
-    pub fn current() -> Token 
+
+    pub fn current(&self) -> Option<&Token>
     {
-        let tstring: String = "Hello".to_string();
-        Token { ttype: TT::NL, tstring:tstring, line_no:1}
+        if self.current_token >= self.tokens.len() {
+            return None;
+        }
+
+        Some(&self.tokens[self.current_token])
     }
 
-    pub fn next() -> Token 
+    pub fn next(&self) -> Option<&Token>
     {
-        let tstring: String = "Hello".to_string();
-        Token { ttype: TT::NL, tstring:tstring, line_no:1}
+        if self.current_token+1 >= self.tokens.len() {
+            return None;
+        }
+
+        Some(&self.tokens[self.current_token+1])
     }
 
     pub fn step(&mut self)
@@ -69,21 +82,101 @@ impl Lexer
 
     pub fn parse(&mut self) -> Vec<u32>
     {
+        let mut instruction_strings = vec!["ADC", "AND", "ASL", "BCC", "BCS", "BEQ", "BIT", "BMI", "BNE", "BPL", "BRK", "BVC", "CLC", "CLD", "CLI", "CLV", "CMP", "CPX", "CPY", "DEC", "DEX", "DEY", "EOR", "INC", "INX", "INY", "JMP", "JSR", "LDA", "LDX", "LDY", "LSR", "NOP", "ORA", "PHA", "PHP", "PLA", "PLP", "ROL", "ROR", "RTI", "RTS", "SBC", "SEC", "SED", "SEI", "STA", "STX", "STY", "TAX", "TAY", "TSX", "TXA", "TXS", "TYA",]; 
+        instruction_strings.sort_unstable();
+
+        // Update unknown tokens and read labels
+        let mut optional = self.current();
+        loop 
+        {
+                let t = self.current();
+
+                if let Some(t) = t
+                {
+                    if t.ttype == TT::UNKNOWN
+                    {
+                        // If instruction
+                        if instruction_strings.contains(&&t.tstring[..])
+                        { 
+                            println!("Found!");
+
+                            self.tokens[self.current_token].ttype = TT::INSTRUCTION;
+                        }
+                        // If label
+                        else if let Some(nt) = self.next()
+                        {
+                            if nt.ttype == TT::COLON
+                            {
+                                self.tokens[self.current_token].ttype = TT::LABEL;
+                            }
+                            else
+                            {
+                                let mut valid = true;
+                                for tc in t.tstring.chars()
+                                {
+                                    if tc < '0' ||  (tc > '9' && tc < 'A') || tc > 'F'
+                                    {
+                                        valid = false;
+                                    }
+                                }
+
+                                if valid
+                                {
+                                    self.tokens[self.current_token].ttype = TT::NUMBER;
+                                }
+                            }
+                        }
+                        // Should we check if operand is a valid number type
+                        else
+                        {
+                            let mut valid = true;
+                            for tc in t.tstring.chars()
+                            {
+                                if tc < '0' ||  (tc > '9' && tc < 'A') || tc > 'F'
+                                {
+                                    valid = false;
+                                }
+                            }
+
+                            if valid
+                            {
+                                self.tokens[self.current_token].ttype = TT::NUMBER;
+                            }
+                        }
+                    }
+
+                    self.step();
+                }
+                else
+                {
+                    break;
+                }
+
+
+        }
+
         let v: Vec<u32> = Vec::new();
+        let _isa = isa::Instruction::ADC;
         v
     }
 
     pub fn tokenize(&mut self, code: String)
     {
         let mut line_no = 1;
+
+        // TODO : Figure this out
+        // unknown_chars.len() had some borrow/copy problems that I don't know how to fix
+        // so instead I used another variable to track the size of the unkown characters
+
         let mut unknown_chars_size = 0;
-        // let mut unknown_string : String = "".to_string();//String::new() ;
         let mut unknown_chars : Vec<char> = Vec::new();//String::new() ;
 
         for c in code.chars()
         {
             if c == ' '
             {
+                // If unknown characters is not empty, then add the token
+                // This check should appear after every known token
                 if unknown_chars_size > 0
                 {
                     let t = Token { ttype: TT::UNKNOWN, tstring:unknown_chars.iter().collect(), line_no:line_no};
@@ -103,10 +196,11 @@ impl Lexer
                     }
                 }
 
-                let t = Token { ttype: TT::WHITE, tstring:" ".to_string(), line_no:line_no};
-
-                self.tokens.push(t);
-                self.step();
+                // THINKABOUT : Do we really need to record whitespaces?
+                //
+                // let t = Token { ttype: TT::WHITE, tstring:" ".to_string(), line_no:line_no};
+                // self.tokens.push(t);
+                // self.step();
 
                 continue;
             }
@@ -179,7 +273,7 @@ impl Lexer
                 continue;
             }
 
-            if c == '%'
+            if c == ','
             {
                 if unknown_chars_size > 0
                 {
@@ -190,7 +284,41 @@ impl Lexer
                     unknown_chars_size = 0; 
                 }
 
-                let t = Token { ttype: TT::PERCENT, tstring:"%".to_string(), line_no:line_no};
+                let t = Token { ttype: TT::COMMA, tstring:",".to_string(), line_no:line_no};
+                self.tokens.push(t);
+                self.step();
+                continue;
+            }
+
+            if c == '('
+            {
+                if unknown_chars_size > 0
+                {
+                    let t = Token { ttype: TT::UNKNOWN, tstring:unknown_chars.iter().collect(), line_no:line_no};
+                    self.tokens.push(t);
+                    self.step();
+                    unknown_chars.clear();
+                    unknown_chars_size = 0; 
+                }
+
+                let t = Token { ttype: TT::BRACKETOPEN, tstring:"(".to_string(), line_no:line_no};
+                self.tokens.push(t);
+                self.step();
+                continue;
+            }
+
+            if c == ')'
+            {
+                if unknown_chars_size > 0
+                {
+                    let t = Token { ttype: TT::UNKNOWN, tstring:unknown_chars.iter().collect(), line_no:line_no};
+                    self.tokens.push(t);
+                    self.step();
+                    unknown_chars.clear();
+                    unknown_chars_size = 0; 
+                }
+
+                let t = Token { ttype: TT::BRACKETCLOSE, tstring:")".to_string(), line_no:line_no};
                 self.tokens.push(t);
                 self.step();
                 continue;
