b91a0b724034d8ab2031221b9f91a1fd6949926b
James Njenga <james.njenga@strathmore.edu>
2021-07-08 12:29:58 +0300

    Update the lexer struct initialization and add better example code

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,32 @@ mod isa;
 mod lexer;
 mod cpu;
 
-use std::io::Write; // bring trait into scope
+use std::collections::HashMap;
+use std::env;
+// use std::io::Write;
 use std::fs;
 
 fn main() {
-    let mut lex = lexer::Lexer {tokens:Vec::new(), current_token:0};
+    let mut lex = lexer::Lexer 
+    {
+        tokens:Vec::new(),
+        current_token:0,
+        labels:HashMap::new()
+    };
 
     lex.tokenize("
     start:
+     jmp start
+     bne start
+     cmp 16
      txa
-     sta $200, y
+     sta $200, X
      sta $300, y
      sta $400, y
      sta $500, y
      iny
      tya
      cmp 16
-     bne do
      iny
      jmp start
     do:
