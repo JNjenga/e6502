ecd1b4d1b3f43b8c829a2ff12793c0befb9eb4f9
James Njenga <james.njenga@strathmore.edu>
2021-07-22 11:27:18 +0300

    SDL implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use std::convert::TryInto;
+use std::time::Duration;
+
 mod isa;
 mod lexer;
 mod cpu;
@@ -9,8 +18,34 @@ use std::env;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
+use rand::Rng;
 
-fn main()
+fn get_pixel_color(value : u8) -> Color
+{
+    match value
+    {
+
+        0 => { return Color::RGB(0, 0, 0); },       // Black
+        1 => { return Color::RGB(255, 255, 255); }, // White
+        2 => { return Color::RGB(255, 0, 0); },     // Red
+        3 => { return Color::RGB(0, 255, 255); },     // Cyan
+        4 => { return Color::RGB(128, 0, 128); },     // Purple
+        5 => { return Color::RGB(0, 255, 0); },     // Green
+        6 => { return Color::RGB(0, 0, 255); },     // Blue
+        7 => { return Color::RGB(255, 255, 0); },     // Yellow
+        8 => { return Color::RGB(255, 165, 0); },     // Orange
+        9 => { return Color::RGB(165, 42, 42); },     // Brown
+        10 => { return Color::RGB(255, 119, 119); },     // Light Red
+        11 => { return Color::RGB(169,169, 169); },     // Dark Grey 
+        12 => { return Color::RGB(128, 128, 128); },     // Grey
+        13 => { return Color::RGB(144, 238, 144); },     // Light green
+        14 => { return Color::RGB(173, 216, 230); },     // Light blue
+        15 => { return Color::RGB(211, 211, 211); },     // Light grey 
+        _ => { return Color::RGB(0, 0, 0); }
+    }
+}
+
+fn main() -> Result<(), String>
 {
     let args: Vec<String> = env::args().collect();
 
@@ -27,7 +62,7 @@ fn main()
         a: 0,
         x: 0,
         y: 0,
-        sp: 0,
+        sp: 0xff,
         pc: 0x600,
         sr: 0,
         mem: [0;1<<16],
@@ -41,13 +76,64 @@ fn main()
     }
 
     // Initialize sdl2
-    // let sdl_context = sdl2::init()?;
-    // let video_subsystem = sdl_context.video()?;
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
 
-    while cpu.pc < ((data.len() as u16) + 0x600)
-    {
-        cpu.step();
-        cpu.print_regs();
-        println!("--------------");
+    let window = video_subsystem
+        .window("6502 Emulator by JNjenga", 800, 600)
+        .resizable()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    let pc_max = data.len() as u16 + 0x600 + 0x12;
+    let mut rng = rand::thread_rng();
+
+    'running: loop {
+        if cpu.pc < pc_max
+        {
+            cpu.mem[0xfe] = rng.gen();
+            cpu.step();
+            // cpu.print_regs();
+
+            canvas.set_draw_color(Color::RGB(255, 0, 0));
+            // Draw monitor
+            for row in 0..32
+            {
+                for col in 0..32
+                {
+                    let color = get_pixel_color(cpu.mem[0x200 + (32 * row + col)]);
+                    canvas.set_draw_color(color);
+                    canvas.fill_rect(Rect::new(col as i32 * 20_i32, row as i32 * 20_i32, 20, 20));
+                }
+            }
+
+            canvas.present();
+        }
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 3));
+        // The rest of the game loop goes here...
     }
+
+    Ok(())
 }
